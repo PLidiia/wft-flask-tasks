0ea870fc2a543ffa124a126a6cc8c311fa170100
PLidiia <151846713+PLidiia@users.noreply.github.com>
2024-02-26 19:36:24 +0700

    Начало Тренировки в полёте

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from flask import Flask
 from flask import render_template
 
+
 app = Flask('__main__')
 
 
-@app.route('/<title>')
-@app.route('/index/<title>')
-def content(title):
-    return render_template('base.html', title=title)
+@app.route('/training/<prof>')
+def content(prof):
+    return render_template('design.html', prof=prof)
 
 
 if __name__ == '__main__':
